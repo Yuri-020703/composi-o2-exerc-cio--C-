8eb7195b17089059e6e03cdeb552d0e442bd3d6e
Richard Yuri <richardyuri930@gmail.com>
2025-03-20 18:52:06 -0300

    Add files via upload

---
+++ Entites/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composição2__Exercício2_.Entites
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
